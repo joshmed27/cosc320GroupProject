76d45b1c8b5b14e1568edaf915aa35df205c6582
Josh Medina <joshuamed27@gmail.com>
2021-03-19 13:28:35 -0600

    Update Method Chain

--- Analysis/methodChain.py
+++ Analysis/methodChain.py
@@ -1,85 +1,30 @@
 import os
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
 
 # Method Chaining
-def load_and_process(path):
+def load_and_processText(path):
     # Chain 1: Data loaded and missing data removed
     df1 = (
-        pd.read_csv(os.path.join('..','..','data','raw','database.csv'))
+        pd.read_csv(os.path.join('..','Dataset','helpfulness_reviews_worst_apps.csv'))
             .dropna()
-            .loc[lambda x: x['Victim Sex'] != 'Unknown']
-            # Not removing victim age at 0 because i realized that could be a baby oops
-            .loc[lambda x: x['Victim Race'] != 'Unknown']
+            .loc[lambda x: x['text'] != '']
+            # Removing no text entries
             .reset_index(drop=True)
     )
-    # Chain 2: Drop irrelevant columns, not sure if we want to add new ones?
+    # Chain 2: Drop irrelevant columns
     df2 = (
         # Currently just dropping the following:
-        # Agency name, Agency Type, Victim & Perp Ethnicity (these columns are somehow different from race - but not.)
-        # Dropping crime type as all these crimes are murder / manslaughter
-        df1.drop(['Record ID','Agency Code', 'Agency Name', 'Crime Type', 'Victim Ethnicity', 'Perpetrator Ethnicity','Record Source'], axis=1)
+        # appTitle, userName, date, score
+        df1.drop(['appTitle', 'userName', 'date', 'score'], axis=1)
             .reset_index(drop=True)
-            .replace({'Month': {'January':1,'Feburary':2,'March':3,'April':4,'May':5,'June':6,
-                                'July':7,'August':8,'September':9,'October':10,'November':11,'December':12}})
-    )
-    return df2
-#    .loc[lambda x: x['Perpetrator Sex'] != 'Unknown']
-#    .loc[lambda x: x['Weapon'] != 'Unknown']
-#    .reset_index(drop=True)
-def weapon_df(df):
-    df2 =(
-        df.groupby(['Perpetrator Sex', 'Weapon']).size()
-        .to_frame(name = 'murder_count').reset_index()
-        .loc[lambda x: x['Perpetrator Sex'] != 'Unknown']
-        .loc[lambda x: x['Weapon'] != 'Unknown']
-        .reset_index(drop=True)
-    ) 
-    return df2
-def race_analysis(df):
-    df2 = (
-        df.groupby(['Victim Race', 'Crime Solved']).size()
-        .to_frame(name = 'Count').reset_index()
-        .loc[lambda x: x['Victim Race'] != 'Unknown']
-        .reset_index(drop=True)
     )
     return df2
 
-def year_analysis(df):
-    df2 = (
-        df.groupby(['Year', 'Crime Solved']).size()
-        .to_frame(name = 'Count').reset_index()
-        .loc[lambda x: x['Year'] != 'Unknown']
-        .reset_index(drop=True)
+def load_and_processSlang(path):
+    # Chain 1: Data loaded and missing data removed
+    df1 = (
+        pd.read_csv(os.path.join('..','Dataset','slangs.csv'))
     )
-    return df2
-def gender_analysis(df):
-    df2 = (
-        df.groupby(['Victim Sex', 'Crime Solved']).size()
-        .to_frame(name = 'Count').reset_index()
-        .loc[lambda x: x['Victim Sex'] != 'Unknown']
-        .reset_index(drop=True)
-    )
-    return df2    
-def racegender_analysis(df):
-    df2 = (
-        df.groupby(['Victim Race','Victim Sex', 'Crime Solved']).size()
-        .to_frame(name = 'Count').reset_index()
-        .loc[lambda x: x['Victim Sex'] != 'Unknown']
-        .loc[lambda x: x['Victim Race']!= 'Unknown']
-        .reset_index(drop=True)  
-    )
-
-    return df2
-
-def victim_age_analysis(df):
-    df2 = (
-        df.groupby(['Victim Age', 'Crime Solved']).size()
-        .to_frame(name = 'Count').reset_index()
-        .loc[lambda x: x['Victim Age'] != 'Unknown']
-        .reset_index(drop=True)
-    )
-    return df2
+    return df1
 
