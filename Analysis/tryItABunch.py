3b6c5f3101cf20b02a0df113ce50bb25e949141b
ramisanaima <ramisanaima@gmail.com>
2021-03-20 04:03:50 +0600

    Create tryItABunch function

---
+++ Analysis/tryItABunch.py
@@ -0,0 +1,31 @@
+import time
+
+# tryItABunch: runs a function a bunch, and times how long it takes.
+#
+# Input: myFn: a function which takes as input a string (tweet)
+# Output: lists nValues and tValues so that running myFn on a string of length nValues[i] took (on average over numTrials tests) time tValues[i] milliseconds.
+#
+# Other optional args:
+#    - startN: smallest n to test
+#    - endN: largest n to test
+#    - stepSize: test n's in increments of stepSize between startN and endN
+#    - numTrials: for each n tests, do numTrials tests and average them
+#    - listMax: the input lists of length n will have values drawn uniformly at random from range(listMax)
+
+def tryItABunch(myFn, startN=10, endN=100, stepSize=10, numTrials=20, listMax = 10):
+    nValues = []
+    tValues = []
+    for n in range(startN, endN, stepSize):
+        # run myFn several times and average to get a decent idea.
+        runtime = 0
+        for t in range(numTrials):
+            tweet = ""
+            start= time.time()
+            myFn(tweet)
+            end=time.time()
+            runtime += (end - start) * 1000 # measure in milliseconds
+	        
+        runtime = runtime/numTrials
+        nValues.append(n)
+        tValues.append(runtime)
+    return nValues, tValues
